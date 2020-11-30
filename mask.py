c3e9252d98e3b1ae6886ece0441e8c02b53da02d
noooyeon <mari970@naver.com>
2020-11-30 17:53:11 +0900

    transformer-train,test code

--- mask.py
+++ mask.py
@@ -1,17 +1,17 @@
 import torch
-def create_mask(src: torch.Tensor,
-                trg: torch.Tensor,
-                src_pad_idx: int,
-                trg_pad_idx: int):
-    src_mask = _create_padding_mask(src, src_pad_idx)
-    trg_mask = None
-    if trg is not None:
-        trg_mask = _create_padding_mask(trg, trg_pad_idx)  # (256, 1, 33)
-        nopeak_mask = _create_nopeak_mask(trg)  # (1, 33, 33)
-        trg_mask = trg_mask & nopeak_mask  # (256, 33, 33)
-    return src_mask, trg_mask
+# def create_mask(src: torch.Tensor,
+#                 trg: torch.Tensor,
+#                 src_pad_idx: int,
+#                 trg_pad_idx: int):
+#     src_mask = _create_padding_mask(src, src_pad_idx)
+#     trg_mask = None
+#     if trg is not None:
+#         trg_mask = _create_padding_mask(trg, trg_pad_idx)  # (256, 1, 33)
+#         nopeak_mask = _create_nopeak_mask(trg)  # (1, 33, 33)
+#         trg_mask = trg_mask & nopeak_mask  # (256, 33, 33)
+#     return src_mask, trg_mask
 
-def _create_padding_mask(seq_q, seq_k, pad_idx: int):
+def create_padding_mask(seq_q, seq_k, pad_idx: int):
     """
     seq 형태를  (256, 33) -> (256, 1, 31) 이렇게 변경합니다.
 
@@ -28,11 +28,11 @@ def _create_padding_mask(seq_q, seq_k, pad_idx: int):
     pad_attn_mask = pad_attn_mask.unsqueeze(1).expand(batch_size, len_q, len_k)
     return pad_attn_mask
 
-def _create_attn_decoder_mask(seq):
+def create_attn_decoder_mask(seq):
     """
     Attention Decoder MASK
     Target의 경우 그 다음 단어를 못보게 가린다
     """
     decoder_mask = torch.one_like(seq).unsqueeze(-1).expand( seq.size(0), seq.size(1), seq.size(1))
-    decoder_mask = decoder_mask.triu(torch.ones(diagonal=1))
+    decoder_mask = decoder_mask.triu(torch.ones(diagonal=1)) # torch.triu: nxn 행렬에서 위쪽 삼각을 리턴
     return decoder_mask
--- transformer.py
+++ transformer.py
@@ -1,13 +1,15 @@
+import torch
 import torch.nn as nn
 from Attention import Embedding, PositionalEncoding, MultiHeadAttention, PositionalFeedForward
+from mask import create_padding_mask, create_attn_decoder_mask
 
-class Encoder_layer(nn.Module):
-    def __init__(self, embed_dim, n_head, dropout_attn, dropout_multi, d_ff, dropout_ff, layernorm_epsilon):
+class EncoderLayer(nn.Module):
+    def __init__(self, emb_dim, n_head, dropout_attn, dropout_multi, d_ff, dropout_ff, layernorm_epsilon):
         super().__init__()
-        self.multiheadattn = MultiHeadAttention(embed_dim, n_head, dropout_attn, dropout_multi)
+        self.multiheadattn = MultiHeadAttention(emb_dim, n_head, dropout_attn, dropout_multi)
         self.normal_layer1 = nn.LayerNorm(eps= layernorm_epsilon)
         self.normal_layer2 = nn.LayerNorm(eps= layernorm_epsilon)
-        self.posffn_layer = PositionalFeedForward(embed_dim, d_ff, dropout_ff)
+        self.posffn_layer = PositionalFeedForward(emb_dim, d_ff, dropout_ff)
 
     def forward(self, enc_input):
         input_q = enc_input
@@ -17,66 +19,108 @@ class Encoder_layer(nn.Module):
         attn_out, attn_prob = self.multiheadattn(input_q, input_k, input_v)
         res_out = residual + attn_out
         out = self.normal_layer1(res_out)
+
         pos_output = self.posffn_layer(out)
         enc_output = self.normal_layer2(pos_output)
+
         return enc_output, attn_prob
 
-class Decoder_layer(nn.Module):
-    def __init__(self, embed_dim, n_head, dropout_attn, dropout_multi,d_ff, dropout_ff, layernorm_epsilon):
+class Encoder(nn.Module):
+    def __init__(self, voca_size, emb_dim, max_seq_len, pos_dropout,dropout_attn, dropout_multi, d_ff, dropout_ff, n_layers, n_head, pad_idx, layernorm_epsilon):
         super().__init__()
-        self.multiheadattn = MultiHeadAttention(embed_dim, n_head, dropout_attn, dropout_multi)
-        self.maskedattn = MultiHeadAttention(embed_dim, n_head, dropout_attn, dropout_multi)
-        self.normal_layer1 = nn.LayerNorm(eps = layernorm_epsilon)
-        self.total_inear = nn.Linear()
-        self.normal_layer2 = nn.LayerNorm(eps = layernorm_epsilon)
-        self.posffn_layer = PositionalFeedForward(embed_dim, d_ff, dropout_ff)
+        self.pad_idx = pad_idx
+        self.embedding_layer = Embedding(voca_size, emb_dim)
+        self.position_layer = PositionalEncoding(max_seq_len, emb_dim, pos_dropout)
+        self.layers = nn.ModuleList([EncoderLayer(emb_dim, n_head, dropout_attn, dropout_multi, d_ff, dropout_ff, layernorm_epsilon) for _ in range(n_layers)])
 
-    def forward(self, dec_input, enc_output):
+    def forward(self, enc_input):
+        emb_enc = self.embedding_layer(enc_input)
+        posemb_enc = self.position_layer(emb_enc)
+        enc_output = posemb_enc
+
+        enc_pad_mask = create_padding_mask(enc_input, enc_input, self.pad_idx)
+
+        enc_attn_prob = []
+
+        for layer in self.layers:
+            enc_output, attn_prob = layer(enc_output, enc_pad_mask)
+            enc_attn_prob.append(attn_prob)
+        return enc_output, enc_attn_prob
+
+
+class DecoderLayer(nn.Module):
+    def __init__(self, emb_dim, n_head, dropout_attn, dropout_multi,d_ff, dropout_ff, layernorm_epsilon):
+        super().__init__()
+        self.multi_head_attn = MultiHeadAttention(emb_dim, n_head, dropout_attn, dropout_multi)
+        self.normal_layer1 = nn.LayerNorm(eps = layernorm_epsilon)
+        self.masked_attn = MultiHeadAttention(emb_dim, n_head, dropout_attn, dropout_multi)
+        self.normal_layer2 = nn.LayerNorm(eps = layernorm_epsilon)
+        self.posffn_layer = PositionalFeedForward(emb_dim, d_ff, dropout_ff)
+        self.normal_layer3 = nn.LayerNorm(eps=layernorm_epsilon)
+
+    def forward(self, dec_input, enc_output, self_attn_mask, enc_dec_mask):
         input_q = dec_input
         input_k = dec_input
         input_v = dec_input
         residual_1 = dec_input
-        attn_out, attn_prob = self.maksedattn(input_q, input_k, input_v)
-        res_out1 =residual_1 + attn_out
-        masked_output = self.regularize_layer(res_out1)
-        residual_2 = masked_output
-        mask_out, mask_prob = self.multiheadattn(input_q, enc_output, enc_output)
-        res_out2 = residual_2 + mask_out
-        pos_output = self.posffn_layer(res_out2)
-        dec_output = self.regularize_layer(pos_output)
-        lin_out = self.total_inear(dec_output)
-        output_prob = nn.Softmax(lin_out)
-        return output_prob,
+        attn_out, attn_prob = self.maksed_attn(input_q, input_k, input_v, self_attn_mask)
+        res_out1 = residual_1 + attn_out
+        masked_output = self.normal_layer1_layer(res_out1)
 
-class Encoder(nn.Module):
-    def __init__(self, voca_size, emb_dim, max_seq_len, pos_dropout, n_layers):
-        super().__init__()
-        self.embedding_layer = Embedding(voca_size, emb_dim)
-        self.position_layer = PositionalEncoding(max_seq_len, emb_dim, pos_dropout)
-        self.layers = nn.ModuleList([Encoder_layer for _ in range(n_layers)])
-    def forward(self):
-        return
+        residual_2 = masked_output
+        mask_out, enc_dec_prob = self.multi_head_attn(masked_output, enc_output, enc_output, enc_dec_mask)
+        res_out2 = residual_2 + mask_out
+        enc_dec_output = self.normal_layer2(res_out2)
+
+        pos_output = self.posffn_layer(enc_dec_output)
+        dec_output = self.normal_layer3(pos_output)
+
+        return dec_output, attn_prob, enc_dec_prob
 
 class Decoder(nn.Module):
-    def __init__(self, voca_size, emb_dim, max_seq_len, pos_dropout, n_layers):
+    def __init__(self, voca_size, emb_dim, max_seq_len, pos_dropout, dropout_attn, dropout_multi, d_ff, dropout_ff, n_layers, n_head, pad_idx, layernorm_epsilon):
         super().__init__()
+        self.pad_idx = pad_idx
         self.embedding_layer = Embedding(voca_size, emb_dim)
         self.position_layer = PositionalEncoding(max_seq_len, emb_dim, pos_dropout)
-        self.layers = nn.ModuleList([Encoder_layer for _ in range(n_layers)])
-    def forward(self):
-        return
+        self.layers = nn.ModuleList([DecoderLayer(emb_dim, n_head, dropout_attn, dropout_multi,d_ff, dropout_ff, layernorm_epsilon) for _ in range(n_layers)])
+        self.total_linear = nn.Linear()
+        self.softmax = nn.Softmax()
 
-class Transformer(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.encoder = Encoder()
-        self.decoder = Decoder()
-
-    def forward(self, enc_input, dec_input):
-        emb_enc = self.embedding_layer(enc_input)
-        posemb_enc = self.position_layer(emb_enc)
-        enc_out = self.encoder(posemb_enc)
+    def forward(self, dec_input, enc_input, enc_output): # memory = seq of output of the last layer of encoder
         emb_dec = self.embedding_layer(dec_input)
         posemb_dec = self.position_layer(emb_dec)
-        output = self.decoder(posemb_dec, enc_out)
-        return output
+        dec_output = posemb_dec
+
+        self_pad_mask = create_padding_mask(dec_input, dec_input, self.pad_idx)
+        self_attn_mask = create_attn_decoder_mask(dec_input)
+        self_mask = torch.gt((self_pad_mask+self_attn_mask), 0) # 첫번째 인풋에 broadcastable한 두번째 아규먼트사용, input>2nd이면 true
+        enc_dec_pad_mask = create_padding_mask(dec_input, enc_input, self.pad_idx)
+
+        dec_self_attn_prob = []
+        enc_dec_attn_prob = []
+
+        for layer in self.layers:
+            dec_output, self_attn, enc_dec_attn = layer(dec_output, enc_output, self_mask, enc_dec_pad_mask)
+            dec_self_attn_prob.append(self_attn)
+            enc_dec_attn_prob.append(enc_dec_attn)
+
+        lin_out = self.total_inear(dec_output)
+        dec_output_prob = self.softmax(lin_out)
+        return dec_output_prob, dec_self_attn_prob, enc_dec_attn_prob
+
+class Transformer(nn.Module): # encoder와 decoder의 emb_dim등은 다르려나?
+    def __init__(self, voca_size, emb_dim, max_seq_len, pos_dropout,dropout_attn,
+                 dropout_multi, d_ff, dropout_ff, n_layers, n_head, pad_idx, layernorm_epsilon):
+        super().__init__()
+        self.encoder = Encoder(voca_size, emb_dim, max_seq_len, pos_dropout,dropout_attn,
+                               dropout_multi, d_ff, dropout_ff, n_layers, n_head, pad_idx, layernorm_epsilon)
+        self.decoder = Decoder(voca_size, emb_dim, max_seq_len, pos_dropout, dropout_attn,
+                               dropout_multi, d_ff, dropout_ff, n_layers, n_head, pad_idx, layernorm_epsilon)
+
+    def forward(self, enc_input, dec_input):
+
+        enc_output, self_enc_attn_prob = self.encoder(enc_input)
+
+        dec_output, self_dec_attn_prob, dec_enc_attn_prob = self.decoder(dec_input, enc_output)
+        return dec_output, self_enc_attn_prob, self_dec_attn_prob, dec_enc_attn_prob
